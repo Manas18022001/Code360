dfebc3249acb03ec7ee015089ffe5c651b0674b6
Manas18022001 <138376110+Manas18022001@users.noreply.github.com>
2025-03-18 23:45:20 +0530

    Add files via upload

---
+++ sortInsertion.cpp
@@ -0,0 +1,17 @@
+//Insertion sort
+
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+    for(int i = 1; i<n; i++){
+        int temp = arr[i];
+        for(int j=i-1; j>=0; j--){
+            if(arr[j]>temp){
+                arr[j+1] = arr[j]; 
+                arr[j] = temp;
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
